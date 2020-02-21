82ea9737812fa998a620096c7ddf23f1d5f5f5c4
José Crespo Guerrero <josecrespoguerrero@gmail.com>
2020-02-21 01:36:29 +0100

    script para lanzar toda las baterias de datos

---
+++ launch_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+dir="data/*"
+
+for file in $dir
+do
+    echo "$file"
+    outputfile=${file#*/}
+    outputfile=${outputfile%%.*}
+    outputfile="output/$outputfile.output"
+    python3 hashcode.py $file > $outputfile
+done
